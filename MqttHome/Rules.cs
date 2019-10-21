45dcfc1e713d375bd0002a1130a86007d94dd083
James Railton <jrailton@gmail.com>
2019-10-21 07:46:08 +0200

    Rules engine -- interim commit

---
+++ MqttHome/Rules.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MqttHome
+{
+    public class RuleEngine {
+
+        private List<Rule> _rules;
+
+        public RuleEngine() {
+            LoadRules();
+        }
+
+        private void LoadRules() {
+            var content = File.ReadAllLines("rules.config");
+
+            foreach (var line in content) { 
+                // omit comment lines and content with comments
+
+            }
+        }
+    }
+    public class Rule
+    {
+    }
+}
