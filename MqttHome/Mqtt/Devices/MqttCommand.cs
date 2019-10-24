e7519e16fca81c65a984c7898f1386483cb852d2
Jimbo <jrailton@gmail.com>
2019-10-24 21:14:04 +0200

    And some other stuff SourceTree just "forgot" to mention

---
+++ MqttHome/Mqtt/Devices/MqttCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace MqttHome.Mqtt
+{
+    public class MqttCommand
+    {
+        public MqttCommand(MqttHomeController controller, string deviceId, string topic, string payload) : this(controller, deviceId, topic, Encoding.UTF8.GetBytes(payload))
+        {
+        }
+
+        public MqttCommand(MqttHomeController controller, string deviceId, string topic, IEnumerable<byte> payload)
+        {
+            Topic = topic;
+            Payload = payload;
+            DeviceId = deviceId;
+            _controller = controller;
+        }
+
+        private MqttHomeController _controller;
+        public string DeviceId { get; set; }
+        public string Topic { get; set; }
+        public IEnumerable<byte> Payload { get; set; }
+
+        public void Execute()
+        {
+            _controller.MqttCommunicator.PublishCommand(this);
+        }
+    }
+}
---
+++ MqttHomeWeb/Controllers/SwitchController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MqttHomeWeb.Controllers
+{
+    public class SwitchController : Controller
+    {
+        public IActionResult Off(string id)
+        {
+            try
+            {
+                Program.MqttHomeController.MqttDevices.Single(d => d.Id == id).SwitchOn("UI Request", null);
+                TempData["success"] = $"Switch {id} was sent a request to switch OFF";
+            }
+            catch (Exception err)
+            {
+                TempData["danger"] = err.Message;
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult On(string id)
+        {
+            try
+            {
+                Program.MqttHomeController.MqttDevices.Single(d => d.Id == id).SwitchOn("UI Request", null);
+                TempData["success"] = $"Switch {id} was sent a request to switch ON";
+            }
+            catch (Exception err)
+            {
+                TempData["danger"] = err.Message;
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
