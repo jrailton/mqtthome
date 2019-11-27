e43d54bf17c4322cdd76d303d8f9d5292142c956
Jimbo <jrailton@gmail.com>
2019-11-27 16:13:31 +0200

    Added write-behind-caching to Influx communicator

--- MqttHome/Influx/InfluxCommunicator.cs
+++ MqttHome/Influx/InfluxCommunicator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using InfluxDB.LineProtocol.Client;
 using InfluxDB.LineProtocol.Payload;
@@ -15,6 +16,11 @@ namespace MqttHome.Influx
         private LineProtocolClient _client;
         private ILog _logger;
 
+        // write behind caching -- to reduce disk activity
+        private LineProtocolPayload _pendingPayload;
+        private Timer _writeBehindTimer;
+        private const int _writeBehindInterval = 5000; // 5 seconds default
+
         public InfluxCommunicator(ILog logger, string influxUrl, string databaseName, string username = null, string password = null)
         {
             _logger = logger;
@@ -23,21 +29,31 @@ namespace MqttHome.Influx
             {
                 var uri = new Uri(influxUrl);
 
-                _logger.Debug($"InfluxCommunicator.ctor :: Connecting to Influx on '{uri.OriginalString}' using database '{databaseName}'...");
+                _logger.Debug($"InfluxCommunicator..ctor :: Connecting to Influx on '{uri.OriginalString}' using database '{databaseName}'...");
 
                 _client = new LineProtocolClient(uri, databaseName, username, password);
+
+                _writeBehindTimer = new Timer(OnWriteBehindTimer, null, _writeBehindInterval, _writeBehindInterval);
             }
             catch (Exception err) {
-                _logger.Error($"InfluxCommunicator.ctor :: Failed to start - {err.Message}", err);
+                _logger.Error($"InfluxCommunicator..ctor :: Failed to start - {err.Message}", err);
                 throw new Exception("Failed to start InfluxCommunicator - see InfluxLog for details");
             }
         }
 
-        public async void Write(LineProtocolPayload payload)
+        private void OnWriteBehindTimer(object o) {
+            if (_pendingPayload != null)
+            {
+                Write(_pendingPayload);
+                _pendingPayload = null;
+            }
+        }
+
+        private void Write(LineProtocolPayload payload)
         {
             try
             {
-                var result = await _client.WriteAsync(payload);
+                var result = _client.WriteAsync(payload).Result;
                 if (!result.Success)
                     throw new Exception($"Failed to write to InfluxDB: {result.ErrorMessage}");
             }
@@ -48,17 +64,11 @@ namespace MqttHome.Influx
 
         public void Write(LineProtocolPoint point)
         {
-            try
-            {
-                var payload = new LineProtocolPayload();
+            // uses write behind caching -- will only write to disk maximum every 5 seconds
+            if (_pendingPayload == null)
+                _pendingPayload = new LineProtocolPayload();
 
-                payload.Add(point);
-
-                Write(payload);
-            }
-            catch (Exception err) {
-                _logger.Error($"Write :: Failed to write to InfluxDB: {err.Message}");
-            }
+            _pendingPayload.Add(point);
         }
     }
 }
